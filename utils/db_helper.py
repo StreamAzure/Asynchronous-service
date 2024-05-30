b367218a01e75c8a2f3589b88035392303ce9629
StreamAzure <1905105036@qq.com>
2024-05-30 10:36:43 +0800

    数据库快照备份&恢复

--- utils/db_helper.py
+++ utils/db_helper.py
@@ -1,33 +1,83 @@
 import os
 import subprocess
-from mysql.connector import errorcode
 import datetime
 import docker
 
-def get_container_id_by_name_pattern(db_instance):
-    client = docker.from_env()
-    containers = client.containers.list()
-    for container in containers:
-        if db_instance in container.name:
-            return container.id
-    return None
+LOG_FILE='backup_log.txt'
+DB_HOST='localhost'
+DB_USER='root'
+DB_PASS='root'
+DB_NAME='ts'
 
-def backup_database(container_id, db_host, db_user, db_pass, db_name, backup_dir):
-    # 获取当前日期时间戳
-    date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    backup_file = os.path.join(backup_dir, f"{db_name}-{date}.sql")
-    log_file = os.path.join(backup_dir, "backup_log.txt")
+def _get_container_id_by_name_pattern(db_instance):
+        client = docker.from_env()
+        containers = client.containers.list()
+        for container in containers:
+            if db_instance in container.name:
+                return container.id
+        raise Exception(f"未找到容器名包含{db_instance}的容器")
 
-    # 确保备份目录存在
-    os.makedirs(backup_dir, exist_ok=True)
+def restore_database(container_name, backup_dir, db_host=DB_HOST, db_user=DB_USER, db_pass=DB_PASS, db_name=DB_NAME):
+    log_file = os.path.join(backup_dir, LOG_FILE)
+    backup_file = None
+
+    # 查找包含指定名称的备份文件
+    for file in os.listdir(backup_dir):
+        if container_name in file:
+            backup_file = os.path.join(backup_dir, file)
+            break
+            
+    container_id = _get_container_id_by_name_pattern(container_name)
 
     # 记录开始时间
     with open(log_file, "a") as log:
-        log.write(f"[{datetime.datetime.now()}] 开始备份数据库 {db_host} {db_name}\n")
+        log.write(f"[{datetime.datetime.now()}] 开始恢复数据库 {container_name} {db_host} {db_name}\n")
+
+    # 在Docker容器中恢复数据库
+    command = (
+        f"docker exec -i {container_id} mysql -u {db_user} -p{db_pass} {db_name} < {backup_file}"
+    )
+    result = subprocess.run(command, shell=True, executable="/bin/bash")
+
+    # 检查恢复是否成功
+    if result.returncode == 0:
+        with open(log_file, "a") as log:
+            log.write(f"[{datetime.datetime.now()}] 数据库恢复成功: {backup_file}\n")
+    else:
+        with open(log_file, "a") as log:
+            log.write(f"[{datetime.datetime.now()}] 数据库恢复失败\n")
+        print("恢复失败")
+        exit(1)
+
+    # 记录结束时间
+    with open(log_file, "a") as log:
+        log.write(f"[{datetime.datetime.now()}] 恢复过程结束\n")
+
+
+def backup_database(container_name, backup_dir, db_host=DB_HOST, db_user=DB_USER, db_pass=DB_PASS, db_name=DB_NAME):
+    """
+    备份容器名为container_name的MySQL数据库
+    备份文件保存在当前目录下
+    """
+    
+    container_id = _get_container_id_by_name_pattern(container_name)
+
+    # 检查目录是否存在，无则创建
+    if not os.path.exists(backup_dir):
+        os.makedirs(backup_dir)
+
+    # 获取当前日期时间戳
+    date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    backup_file = os.path.join(backup_dir, f"{container_name}_{db_name}_{date}.sql")
+    log_file = os.path.join(backup_dir, LOG_FILE)
+
+    # 记录开始时间
+    with open(log_file, "a") as log:
+        log.write(f"[{datetime.datetime.now()}] 开始备份数据库 {container_name} {db_host} {db_name}\n")
 
     # 在Docker容器中备份数据库
     command = (
-        f"docker exec {container_id} mysqldump -u {db_user} -p {db_pass} {db_name} > {backup_file}"
+        f"docker exec {container_id} mysqldump -u {db_user} -p{db_pass} {db_name} > {backup_file}"
     )
     result = subprocess.run(command, shell=True, executable="/bin/bash")
 
@@ -46,5 +96,6 @@ def backup_database(container_id, db_host, db_user, db_pass, db_name, backup_dir
         log.write(f"[{datetime.datetime.now()}] 备份过程结束\n")
 
 if __name__ == "__main__":
-    container_id = get_container_id_by_name_pattern("ts-order-other-mysql")
-    backup_database(container_id, "localhost", "root", "root", "ts", "/home/backup")
+    container_name = "ts-order-other-mysql"
+    backup_database(container_name, "./replay_backup")
+    restore_database(container_name, "./replay_backup")
